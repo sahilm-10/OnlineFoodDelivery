7b5f228b42e5c68a7c8c9edf03372072e1faf2b3
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2022-03-30 13:34:58 +0530

    Update and rename sw.js to app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-<script>
- if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('sw.js')
       .then(function(reg){
         console.log("Yes, it did.");
      }).catch(function(err) {
         console.log("No it didn't. This happened:", err)
     });
  }
-</script>
+
