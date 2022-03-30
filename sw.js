a0854cbc921b8bb778056f16b3d69294350b8b00
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2022-03-30 14:07:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,3 +11,43 @@ self.addEventListener('activate', function(event) {
 self.addEventListener("fetch", event => {
     console.log("You fetched " + event.url);
 });
+
+const assets = [
+    "/style.css",
+    "/index.css",
+    "/index.html",
+    "/dummy.html",
+    "/"
+];
+
+
+let cache_name = "SimiCart"; // The string used to identify our cache
+
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(cache_name)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+            .catch(err => console.log(err))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.url === "https://onlinefooddeliverynew.herokuapp.com/homepage.php") {
+        // or whatever your app's URL is
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
+});
