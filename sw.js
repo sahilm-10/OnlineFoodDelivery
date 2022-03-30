ed1bb307dfa1edb7f2830216a93ccf5ce80306a2
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2022-03-30 13:59:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,24 @@ self.addEventListener('install', function(event) {
     console.log("Installed");
 });
 
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/dummy.html',
+       '/fooditem.css',
+       '/receipe.html',
+       '/index.css',
+     ]);
+   })
+ );
+});
+
+
 self.addEventListener('activate', function(event) {
    console.log("Activated");
 });
