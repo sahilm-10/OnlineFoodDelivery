c08f4dec0e6b9eeb51179237e3a9f005811ba0db
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2022-03-30 13:40:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('sw.js')
-      .then(function(reg){
-        console.log("Yes, it did.");
-     }).catch(function(err) {
-        console.log("No it didn't. This happened:", err)
-    });
- }
-
+  navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
