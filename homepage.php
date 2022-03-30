96f38f3e66f9fd56bcb7685aa53f70771ef1b4e4
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2022-03-30 13:33:24 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -410,5 +410,15 @@
 
 
 </body>
-
+<script>
+ if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('sw.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+     }).catch(function(err) {
+        console.log("No it didn't. This happened:", err)
+    });
+ }
+</script>
 </html>
